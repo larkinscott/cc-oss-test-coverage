96883d075432a4079ba7adb4a6c2d2cf57543c1f
Gordon Diggs <gordon@gordondiggs.com>
2016-11-10 10:51:23 -0500

    succeed

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -3,7 +3,7 @@ require "dog"
 RSpec.describe Dog do
   describe "#name" do
     it "is super excited about its name" do
-      expect(Dog.new("Milo").name).to eq("Milsdfdfo!!!")
+      expect(Dog.new("Milo").name).to eq("Milo!!!")
     end
   end
 end
